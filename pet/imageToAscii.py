100bf69a3eedbd915ec92f38ce8a342fd34bd4bd
Sur091 <sacharya27@wooster.edu>
2024-10-04 12:06:57 -0400

    Successfully implemented image to ASCII feature.

--- pet/imageToAscii.py
+++ pet/imageToAscii.py
@@ -1,5 +1,8 @@
 
+# from typing_extensions import Optional
+import cv2
 
+import os
 ### This module should be able to convert a given image into ascii art 
 ### Input:
 ### 1 .Need to know the number of characters that can be printed on the screen
@@ -7,12 +10,64 @@
 
 
 class Image:
+    
+    CHARACTERS: str = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
+    
     def __init__(self, file_path: str) -> None:
-        self.file_path = file_path
+        self.file_path: str = file_path
         
-    def print_file_path(self) -> str:
+        self.image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+        
+        # print(type(self.image))
+        
+        # if self.image is not None: print("Success in loading image")
+        
+    def get_file_path(self) -> str:
         return self.file_path
         
         
-    def print_image(self) -> str:
-        return self.file_path
+    def show_image(self) -> str:
+        # Display the image
+        cv2.imshow("Image", self.image)
+        
+        # Wait for the user to press a key
+        cv2.waitKey(0)
+        
+        # Close all windows
+        cv2.destroyAllWindows()
+        
+        return "success"
+    
+    def get_image_string(self) -> str:
+        if self.image is None:
+            return "Could not read the image."
+            
+        
+        terminal_character_size = os.get_terminal_size()
+        
+        size_of_image_in_terminal  = 0.8
+        
+        width_of_resclaed_image = int(os.get_terminal_size()[0] * size_of_image_in_terminal)
+        height_of_rescaled_image = width_of_resclaed_image // 3
+        
+        rescaled_image_size = width_of_resclaed_image, height_of_rescaled_image
+        
+        rescaled_image = cv2.resize(self.image, rescaled_image_size)
+        
+        image_string = ""
+        
+        for i, row_of_image in enumerate(rescaled_image):
+            for j, grey_scale_value in enumerate(row_of_image):
+                character = self.get_character_for_gery_scale_value(grey_scale_value)
+                image_string += character
+            image_string += '\n'
+        
+        return image_string
+        
+    
+    def get_character_for_gery_scale_value(self, grey_scale_value: int) -> str:
+        normalized_grey_scale_value = grey_scale_value / 256.0
+        index_of_ascii_character = int(len(self.CHARACTERS) * normalized_grey_scale_value)
+        corresponding_ascii_character = self.CHARACTERS[index_of_ascii_character]
+        
+        return corresponding_ascii_character
